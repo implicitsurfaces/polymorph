45b9837830b14eeb480286840fbe19aaca462672
Steve Genoud <steve@sgenoud.com>
2024-04-29 14:43:58 +0200

    Refactor the code for use in the visualization

--- crates/buoyancy/src/bin/float.rs
+++ crates/buoyancy/src/bin/float.rs
@@ -1,84 +1,14 @@
-// Explore GEO APIs needed to create a shape, calculate its center of gravity, drop it into water and calculate intersections, etc.
-//
-// |            |
-// |            |
-// -------------- waterline at y = 0; y from +/- 1
-// |            |
-// |____________| x from +/- 1
+use buoyancy::Simulation;
+use geo::{Centroid, Polygon};
 
-use geo::{Area, BooleanOps, Centroid, Coord, HasDimensions, Polygon, Rotate, Translate};
-
-pub fn main() {
-    use geo::polygon;
-
-    let water = polygon![
-            (x: -1.0, y: 0.0),
-            (x: 1.0, y: 0.0),
-            (x: 1.0, y: -1.0),
-            (x: -1.0, y: -1.0),
-    ];
-
-    // a square initially right above the waterline
-    let length: f64 = 0.5;
-    let mut boat = polygon![
-        (x: 0.0, y: 0.0),
-        (x: 0.0, y: length),
-        (x: length, y: length),
-        (x: length, y: 0.0),
-    ];
-
-    ///////////////////
-    // Constants
-
-    let density_water = 1.0;
-    let density_boat = 0.5;
-    let gravity = 1.0;
-    let tolerance = 1e-5;
-
-    /////////////////////////////////////////////
-    // Iterate until force and torque disappear (i.e., stability)
-    // Note that each iteration isn't a physically accurate time step.
-    // ChatGPT wants to do that, but most of it is unnecessary for our application;  I simplified so we have a minimal simulation
-    // See: https://chat.openai.com/share/e3999208-d2c1-4162-808e-e70bbe32951c
-
-    let mut converged = false;
-    let mut iterations = 0;
-
-    while !converged && iterations < 1000 {
-        let displacement = water.intersection(&boat);
-
-        let center_of_gravity = boat.centroid().unwrap();
-        let center_of_buoyancy = displacement.centroid().unwrap();
-
-        // println!("CoG: {center_of_gravity:?}");
-        // println!("CoB: {center_of_buoyancy:?}");
-
-        // Calculate net vertical force
-        let boat_mass = density_boat * boat.unsigned_area();
-        let force_gravity = -gravity * boat_mass;
-        let force_buoyancy = density_water * displacement.unsigned_area() * gravity;
-        let force_net = force_buoyancy + force_gravity;
-        let dy = force_net / (boat_mass * gravity);
-
-        // Calculate torque
-        let distance_vector = Coord {
-            x: center_of_buoyancy.x() - center_of_gravity.x(),
-            y: center_of_buoyancy.y() - center_of_gravity.y(),
-        };
-        let torque = distance_vector.x * force_buoyancy; // Simplified 2D torque about z-axis
-        let moment_of_inertia = 1.0; // We don't need this since we don't care about making the simulation physical.
-        let angular_adjustment = torque / moment_of_inertia;
-
-        boat.translate_mut(0., dy);
-        boat.rotate_around_point_mut(angular_adjustment, center_of_gravity);
-
-        if force_net.abs() < tolerance && torque.abs() < tolerance {
-            converged = true;
+fn main() {
+    let simulation = Simulation::new();
+    match simulation.run() {
+        Some(results) => {
+            println!("Final position: {:?}", results.centroid().unwrap());
         }
-
-        iterations += 1;
+        None => {
+            println!("Simulation did not converge.");
+        }
     }
-    println!("Converged? {converged}");
-
-    println!("Boat: {boat:?}");
 }
--- crates/buoyancy/src/lib.rs
+++ crates/buoyancy/src/lib.rs
@@ -1,14 +1,125 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use geo::polygon;
+use geo::{
+    Area, BooleanOps, BoundingRect, Centroid, Coord, MultiPolygon, Polygon, Rect, Rotate, Translate,
+};
+
+pub struct SimulationResults {
+    dy: f64,
+    angular_adjustment: f64,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl SimulationResults {
+    pub fn is_not_moving(&self, tolerance: f64) -> bool {
+        return self.dy.abs() < tolerance && self.angular_adjustment.abs() < tolerance;
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct Simulation {
+    boat: Polygon<f64>,
+    density_water: f64,
+    density_boat: f64,
+    gravity: f64,
+    tolerance: f64,
+}
+
+impl Simulation {
+    pub fn new() -> Self {
+        let length: f64 = 0.5;
+        let boat = polygon![
+            (x: 0.0, y: 0.0),
+            (x: 0.0, y: length),
+            (x: length, y: length),
+            (x: length, y: 0.0),
+        ];
+
+        Self {
+            boat,
+            density_water: 1.0,
+            density_boat: 0.6,
+            gravity: 1.0,
+            tolerance: 1e-5,
+        }
+    }
+
+    pub fn underwater_volume(&self, boat: &Polygon<f64>, water_level: f64) -> MultiPolygon<f64> {
+        let boat_bounding_box = boat.bounding_rect().unwrap();
+        let water = Rect::new(
+            boat_bounding_box.min(),
+            Coord {
+                x: boat_bounding_box.max().x,
+                y: water_level,
+            },
+        )
+        .to_polygon();
+
+        return water.intersection(&self.boat);
+    }
+
+    pub fn compute_forces(&self, boat: &Polygon<f64>) -> SimulationResults {
+        let displacement = self.underwater_volume(boat, 0.0);
+
+        let center_of_gravity = boat.centroid().unwrap();
+
+        let center_of_buoyancy = match displacement.centroid() {
+            Some(coord) => coord,
+            None => center_of_gravity,
+        };
+
+        println!("CoG: {:?}", center_of_gravity.y());
+        //println!("CoB: {center_of_buoyancy:?}");
+
+        // Calculate net vertical force
+        let boat_mass = self.density_boat * boat.unsigned_area();
+        let force_gravity = -self.gravity * boat_mass;
+        let force_buoyancy = self.density_water * displacement.unsigned_area() * self.gravity;
+
+        let force_net = force_buoyancy + force_gravity;
+        let dy = force_net / (boat_mass * self.gravity);
+
+        // Calculate torque
+        let distance_vector = Coord {
+            x: center_of_buoyancy.x() - center_of_gravity.x(),
+            y: center_of_buoyancy.y() - center_of_gravity.y(),
+        };
+        let torque = distance_vector.x * force_buoyancy; // Simplified 2D torque about z-axis
+        let moment_of_inertia = 1.0; // We don't need this since we don't care about making the simulation physical.
+        let angular_adjustment = torque / moment_of_inertia;
+
+        return SimulationResults {
+            dy,
+            angular_adjustment,
+        };
+    }
+
+    pub fn apply_forces(&self, boat: &mut Polygon<f64>, simulation_results: SimulationResults) {
+        let center_of_gravity = boat.centroid().unwrap();
+        boat.rotate_around_point_mut(simulation_results.angular_adjustment, center_of_gravity);
+
+        boat.translate_mut(0.0, simulation_results.dy);
+    }
+
+    pub fn run(&self) -> Option<Polygon<f64>> {
+        let mut converged = false;
+        let mut iterations = 0;
+
+        let mut boat = self.boat.clone();
+
+        while !converged && iterations < 1000 {
+            let simulation_results = self.compute_forces(&boat);
+
+            if simulation_results.is_not_moving(self.tolerance) {
+                converged = true;
+                break;
+            }
+
+            self.apply_forces(&mut boat, simulation_results);
+            iterations += 1;
+        }
+
+        if !converged {
+            None
+        } else {
+            Some(boat)
+        }
     }
 }

10955b213d2b1323fbbfdb6563897c71d9315641
Patrick Dubroy <pdubroy@gmail.com>
2024-05-10 11:03:01 +0200

    Clean up viz scripts
    - Replace the two scripts (plain heat map, + 3D one) with a single
      script.
    - Make the script take the CSV filename as an argument.
    - Update the README

--- script/equilibrium_viz.py
+++ script/equilibrium_viz.py
@@ -1,12 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-import mplcursors
+import sys
+import os
+
+three_d = '--3d' in sys.argv
+
+if len(sys.argv) < 2 or not os.path.exists(sys.argv[-1]):
+    print(f'usage: python {sys.argv[0]} [--3d] <filename>')
+    sys.exit(1)
+
+filename = sys.argv[-1]
 
 # Read the cost function data from a CSV file
 parameters = []
 costs = []
-with open('cost_function_data.csv', 'r') as file:
+with open(filename, 'r') as file:
     csv_reader = csv.reader(file)
     next(csv_reader)  # Skip the header row if present
     for row in csv_reader:
@@ -22,16 +31,17 @@ costs = np.array(costs)
 param1_values = parameters[:, 0]
 param2_values = parameters[:, 1]
 
-# Create a scatter plot
-fig, ax = plt.subplots()
-scatter = ax.scatter(param1_values, param2_values, c=costs, cmap='viridis', s=10)
+# Create a 3D scatter plot
+if three_d:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    scatter = ax.scatter(param1_values, param2_values, costs, c=costs, cmap='viridis', s=1)
+    ax.set_zlabel('Cost')
+else:
+    fig, ax = plt.subplots()
+    scatter = ax.scatter(param1_values, param2_values, c=costs, cmap='viridis', s=1)
 fig.colorbar(scatter, label='Cost')
 ax.set_xlabel('Y position')
 ax.set_ylabel('Rotation angle')
-ax.set_title('Scatter Plot of Cost Function')
-
-# Enable hover annotations
-cursor = mplcursors.cursor(scatter, hover=True)
-cursor.connect("add", lambda sel: sel.annotation.set_text(f"Cost: {sel.target[2]:.2f}"))
 
 plt.show()

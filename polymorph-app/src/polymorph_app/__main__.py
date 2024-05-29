0590b44cd397274feec8400d130b5423842a803a
Patrick Dubroy <pdubroy@gmail.com>
2024-05-29 15:03:59 +0200

    polymorph_app: Render shapes via blit_array

--- polymorph-app/src/polymorph_app/__main__.py
+++ polymorph-app/src/polymorph_app/__main__.py
@@ -1,7 +1,12 @@
+import jax
+import jax.numpy as jnp
 import pygame
+import time
 
 from pygame_gui import UIManager, UI_BUTTON_PRESSED
 from pygame_gui.elements import UIButton
+from pygame import surfarray
+from polymorph_s2df import *
 
 # Constants
 FPS = 60
@@ -9,28 +14,46 @@ WIDTH, HEIGHT = 800, 600
 BACKGROUND_COLOR = (255, 255, 255)  # White background
 
 
-def render_scene(surface):
-    surface.fill(BACKGROUND_COLOR)
+def gen_pixel_grid():
+    xx, yy = jnp.meshgrid(jnp.arange(HEIGHT), jnp.arange(WIDTH))
+    return jnp.stack((xx, yy), axis=-1)
+
+
+pixel_grid = gen_pixel_grid()
+
+
+def render_scene(sdf):
     color = (255, 0, 0)
-    pos = (WIDTH / 2, HEIGHT / 2)
-    radius = 100
-    pygame.draw.circle(surface, color, pos, radius)
+
+    start = time.time()
+    ans = jax.vmap(jax.vmap(sdf.is_inside, in_axes=0), in_axes=0)(pixel_grid)
+    # ans = sdf.is_inside(pixel_grid)
+    # print(f"rendered in {time.time() - start}s")
+
+    # Convert the float entries for each pixel into a (3,) of uint8.
+    ans_3d = jnp.repeat(ans[:, :, jnp.newaxis], 3, axis=2)
+    return (ans_3d * 255).astype(jnp.uint8)
 
 
 if __name__ == "__main__":
+    window_dims = (WIDTH, HEIGHT)
     pygame.init()
 
     pygame.display.set_caption("Quick Start")
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    ui_manager = UIManager((800, 600))
-
-    background = pygame.Surface((800, 600))
-    background.fill(ui_manager.ui_theme.get_colour("dark_bg"))
+    display_surface = pygame.display.set_mode(window_dims)
+    ui_manager = UIManager(window_dims)
 
     hello_button = UIButton((350, 280), "Hello")
 
     clock = pygame.time.Clock()
     is_running = True
+
+    scene = (
+        Circle(200)
+        .substract(Circle(50))
+        .substract(Box(230, 20).translate(p(100, 0)))
+        .translate(p(200, 200))
+    )
 
     while is_running:
         time_delta = clock.tick(FPS) / 1000.0
@@ -43,7 +66,7 @@ if __name__ == "__main__":
             ui_manager.process_events(event)
         ui_manager.update(time_delta)
 
-        render_scene(screen)
-        ui_manager.draw_ui(screen)
+        surfarray.blit_array(display_surface, render_scene(scene))
+        ui_manager.draw_ui(display_surface)
 
         pygame.display.update()

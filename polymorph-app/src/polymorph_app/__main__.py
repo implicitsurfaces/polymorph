67c83c8d709ec91265f29130cd2b388db89ed5b2
Patrick Dubroy <pdubroy@gmail.com>
2024-05-30 14:35:03 +0200

    polymorph_app: Add ability to draw
    Label in the bottom right shows the current tool.
    Press ‘c’ to activate the Circle tool.
    Press ‘b’ to activate the Box tool.

--- polymorph-app/src/polymorph_app/__main__.py
+++ polymorph-app/src/polymorph_app/__main__.py
@@ -1,10 +1,12 @@
+from collections import namedtuple
+from functools import reduce
 import jax
 import jax.numpy as jnp
 import pygame
 import time
 
 from pygame_gui import UIManager, UI_BUTTON_PRESSED
-from pygame_gui.elements import UIButton
+from pygame_gui.elements import *
 from pygame import surfarray
 from polymorph_s2df import *
 
@@ -12,6 +14,13 @@ from polymorph_s2df import *
 FPS = 60
 WIDTH, HEIGHT = 800, 600
 BACKGROUND_COLOR = (255, 255, 255)  # White background
+
+# Maps from a pygame key constant (https://www.pygame.org/docs/ref/key.html#key-constants-label)
+# to a s2df constructor.
+TOOL_HOTKEYS = {pygame.K_c: Circle, pygame.K_b: Box, pygame.K_t: Triangle}
+
+
+Gesture = namedtuple("Gesture", ["start_pos", "shapes"])
 
 
 def gen_pixel_grid():
@@ -42,30 +51,61 @@ if __name__ == "__main__":
     display_surface = pygame.display.set_mode(window_dims)
     ui_manager = UIManager(window_dims)
 
-    hello_button = UIButton((350, 280), "Hello")
+    button_layout_rect = pygame.Rect(0, 0, 100, 20)
+    button_layout_rect.bottomright = (-30, -20)
+
+    tool_label = UILabel(
+        relative_rect=button_layout_rect,
+        text="",
+        manager=ui_manager,
+        container=ui_manager.root_container,
+        anchors={"right": "right", "bottom": "bottom"},
+    )
 
     clock = pygame.time.Clock()
     is_running = True
+
+    gesture = None
+    shapes = []
+    tool = None
 
     while is_running:
         time_delta = clock.tick(FPS) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
-            if event.type == UI_BUTTON_PRESSED:
-                if event.ui_element == hello_button:
-                    print("Hello World!")
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                gesture = Gesture(p(*event.pos), [])
+            elif event.type == pygame.MOUSEBUTTONUP:
+                shapes = shapes + gesture.shapes
+                gesture = None
+            elif event.type == pygame.KEYDOWN:
+                if event.key in TOOL_HOTKEYS:
+                    tool = TOOL_HOTKEYS[event.key]
             ui_manager.process_events(event)
         ui_manager.update(time_delta)
+        tool_label.set_text(tool.__name__ if tool else "None")
 
-        mouse_pos = pygame.mouse.get_pos()
+        keys = pygame.key.get_pressed()
 
-        scene = (
-            Circle(200)
-            .substract(Circle(50))
-            .substract(Box(230, 20).translate(p(100, 0)))
-            .translate(p(*mouse_pos))
-        )
+        if gesture and tool:
+            current_pos = p(*pygame.mouse.get_pos())
+            if tool == Circle:
+                r = jnp.linalg.norm(current_pos - gesture.start_pos, axis=-1)
+                gesture = Gesture(
+                    gesture.start_pos, [tool(r).translate(gesture.start_pos)]
+                )
+            elif tool == Box:
+                w, h = (jnp.abs(current_pos - gesture.start_pos)) * 2
+                gesture = Gesture(
+                    gesture.start_pos, [tool(w, h).translate(gesture.start_pos)]
+                )
+
+        if keys[pygame.K_c]:
+            pass
+
+        gesture_shapes = gesture.shapes if gesture else []
+        scene = reduce(lambda a, b: Union(a, b), shapes + gesture_shapes, Circle(0))
 
         surfarray.blit_array(display_surface, render_scene(scene))
         ui_manager.draw_ui(display_surface)

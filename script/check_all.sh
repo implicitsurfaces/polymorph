a5519b84661da10e4228058bc178bc5ee12bb73a
Kevin J. Lynagh <kevin@keminglabs.com>
2024-06-05 10:06:27 +0200

    Disable Rust checks for now since we're all working in Python.

--- script/check_all.sh
+++ script/check_all.sh
@@ -6,12 +6,12 @@ DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "$DIR/../"
 
-# Keep people from accidentally pushing unformatted code.
-cargo fmt --check
+# # Keep people from accidentally pushing unformatted code.
+# cargo fmt --check
 
-cargo --locked clippy --                   \
-      -D warnings                          \
-      -A clippy::new-without-default
+# cargo --locked clippy --                   \
+#       -D warnings                          \
+#       -A clippy::new-without-default
 
 if type rye >/dev/null 2>&1; then
     rye run ruff check --fix script/*.py

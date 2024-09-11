f4126536a9aaa23cac7bcf911b2442b584ef9910
Max Bernstein <max@bernsteinbear.com>
2024-09-11 16:09:39 -0400

    .

--- test.py
+++ test.py
@@ -30,35 +30,6 @@ solid = (
 
 grid_x, grid_y, grid_z = grid_gen_3d(100, 100, 100)
 expr = solid.distance(grid_x, grid_y, grid_z)
-# side_length = ir.Param(0)
-# corners = [
-#     [ir.Scalar(0), ir.Scalar(0)],
-#     [ir.Scalar(0), side_length],
-#     [side_length, side_length],
-#     [side_length, ir.Scalar(0)],
-# ]
-# def distance(p0, p1):
-#     return ir.Unary(
-#             ir.Binary(
-#                 ir.Binary(
-#                     ir.Binary(p0[0], p1[0], ir.BinOp.Sub),
-#                     ir.Binary(p0[0], p1[0], ir.BinOp.Sub),
-#                     ir.BinOp.Mul),
-#                 ir.Binary(
-#                     ir.Binary(p0[1], p1[1], ir.BinOp.Sub),
-#                     ir.Binary(p0[1], p1[1], ir.BinOp.Sub),
-#                     ir.BinOp.Mul),
-#                 ir.BinOp.Add),
-#         ir.UnOp.Sqrt,
-#         (),
-#     )
-# expr = distance(corners[0], corners[1]) + \
-#         distance(corners[1], corners[2]) + \
-#         distance(corners[2], corners[3]) + \
-#         distance(corners[3], corners[0]) + \
-#         distance(corners[0], corners[2]) + \
-#         distance(corners[1], corners[3])
-
 after = time.perf_counter()
 print(f"Build IR: {after - before:.2f}s", file=sys.stderr)
 

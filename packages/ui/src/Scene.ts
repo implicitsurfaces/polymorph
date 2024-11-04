1c61c03ca3267a52a38608a9b6c3e38d16691541
Boris Dalstein <dalboris@gmail.com>
2024-11-04 15:33:58 +0100

    ui: add clone(), copy(), and method chaining to Scene class

--- packages/ui/src/Scene.ts
+++ packages/ui/src/Scene.ts
@@ -6,8 +6,27 @@ import { Vector2 } from 'threejs-math';
 export class Scene {
   constructor(public points: Array<Vector2> = []) {}
 
-  addPoint(position: Vector2) {
+  /**
+   * Returns a new scene with the same content as this one.
+   */
+  clone(): Scene {
+    return new this.constructor().copy(this);
+  }
+
+  /**
+   * Copies the content from the source scene into this one.
+   */
+  copy(source: Scene): Scene {
+    this.points = [...source.points];
+    return this;
+  }
+
+  /**
+   * Adds a point to the scene.
+   */
+  addPoint(position: Vector2): Scene {
     this.points.push(position);
+    return this;
   }
 }
 

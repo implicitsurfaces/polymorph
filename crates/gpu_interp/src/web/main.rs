110b94620107b4d09aa9dc614a074e9bf5040e91
Kevin J. Lynagh <kevin@keminglabs.com>
2024-11-15 11:18:28 +0100

    gpu_interp web: fix canvas to 640 x 640 px; use `event_loop.spawn` to eliminate exception as control flow noise from the console.

--- crates/gpu_interp/src/web/main.rs
+++ crates/gpu_interp/src/web/main.rs
@@ -24,8 +24,12 @@ use winit::{
 };
 
 async fn run(event_loop: EventLoop<()>, window: Window) {
+    // Make ourselves a 'static Window we can reference from the event loop so we don't need this noise https://users.rust-lang.org/t/wgpu-winit-weird-exception/92604
+    static mut WINDOW: Option<Window> = None;
+    unsafe { WINDOW = Some(window) }
+    let window = unsafe { WINDOW.as_ref().unwrap() };
     let instance = wgpu::Instance::default();
-    let surface = instance.create_surface(&window).unwrap();
+    let surface = instance.create_surface(window).unwrap();
     let options = wgpu::RequestAdapterOptions {
         power_preference: wgpu::PowerPreference::default(),
         force_fallback_adapter: false,
@@ -44,9 +48,12 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     }
     let tree = smooth_union(circles);
 
+    let window_size = window.inner_size();
+
+    // viewport should be closest multiple of tile size
     let viewport = Viewport {
-        width: 64 * 20,
-        height: 64 * 10,
+        width: (window_size.width / TILE_SIZE_X) * TILE_SIZE_X,
+        height: (window_size.height / TILE_SIZE_Y) * TILE_SIZE_Y,
     };
 
     let mut ctx = Context::new();
@@ -121,92 +128,85 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         .unwrap();
     surface.configure(&device, &config);
 
-    let window = &window;
-
     let mut step_count = 0;
+    use winit::platform::web::EventLoopExtWebSys;
+    event_loop.spawn(move |event, target| {
+        let _ = (&instance, &adapter);
 
-    event_loop
-        .run(move |event, target| {
-            let _ = (&instance, &adapter);
+        if let Event::WindowEvent {
+            window_id: _,
+            event,
+        } = event
+        {
+            match event {
+                WindowEvent::Resized(new_size) => {
+                    let max_texture_size = device.limits().max_texture_dimension_2d;
+                    config.width = new_size.width.max(1).min(max_texture_size);
+                    config.height = new_size.height.max(1).min(max_texture_size);
 
-            if let Event::WindowEvent {
-                window_id: _,
-                event,
-            } = event
-            {
-                match event {
-                    WindowEvent::Resized(new_size) => {
-                        let max_texture_size = device.limits().max_texture_dimension_2d;
-                        config.width = new_size.width.max(1).min(max_texture_size);
-                        config.height = new_size.height.max(1).min(max_texture_size);
+                    surface.configure(&device, &config);
+                    window.request_redraw();
+                }
+                WindowEvent::RedrawRequested => {
+                    // The step count is a "logical time" that is updated
+                    // every frame.
+                    step_count += 1;
+                    queue.write_buffer(
+                        &buffers.step_count_buffer,
+                        0,
+                        bytemuck::cast_slice(&[step_count]),
+                    );
 
-                        surface.configure(&device, &config);
-                        window.request_redraw();
+                    let frame = surface
+                        .get_current_texture()
+                        .expect("Failed to acquire next swap chain texture");
+                    let view = frame
+                        .texture
+                        .create_view(&wgpu::TextureViewDescriptor::default());
+
+                    let mut encoder = device
+                        .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+                    add_compute_pass(
+                        &mut encoder,
+                        &compute_pipeline,
+                        &bind_group,
+                        None,
+                        &viewport,
+                    );
+
+                    // dropped and unlocks the encoder when we're done
+                    // with it.
+                    {
+                        let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                            label: None,
+                            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                                view: &view,
+                                resolve_target: None,
+                                ops: wgpu::Operations {
+                                    load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
+                                    store: wgpu::StoreOp::Store,
+                                },
+                            })],
+                            depth_stencil_attachment: None,
+                            timestamp_writes: None,
+                            occlusion_query_set: None,
+                        });
+                        rpass.set_pipeline(&render_pipeline);
+                        rpass.set_bind_group(0, &bind_group, &[]);
+                        rpass.draw(0..6, 0..1);
                     }
-                    WindowEvent::RedrawRequested => {
-                        // The step count is a "logical time" that is updated
-                        // every frame.
-                        step_count += 1;
-                        queue.write_buffer(
-                            &buffers.step_count_buffer,
-                            0,
-                            bytemuck::cast_slice(&[step_count]),
-                        );
 
-                        let frame = surface
-                            .get_current_texture()
-                            .expect("Failed to acquire next swap chain texture");
-                        let view = frame
-                            .texture
-                            .create_view(&wgpu::TextureViewDescriptor::default());
+                    queue.submit(Some(encoder.finish()));
+                    frame.present();
 
-                        let mut encoder =
-                            device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                                label: None,
-                            });
-
-                        add_compute_pass(
-                            &mut encoder,
-                            &compute_pipeline,
-                            &bind_group,
-                            None,
-                            &viewport,
-                        );
-
-                        // dropped and unlocks the encoder when we're done
-                        // with it.
-                        {
-                            let mut rpass =
-                                encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                                    label: None,
-                                    color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                                        view: &view,
-                                        resolve_target: None,
-                                        ops: wgpu::Operations {
-                                            load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
-                                            store: wgpu::StoreOp::Store,
-                                        },
-                                    })],
-                                    depth_stencil_attachment: None,
-                                    timestamp_writes: None,
-                                    occlusion_query_set: None,
-                                });
-                            rpass.set_pipeline(&render_pipeline);
-                            rpass.set_bind_group(0, &bind_group, &[]);
-                            rpass.draw(0..6, 0..1);
-                        }
-
-                        queue.submit(Some(encoder.finish()));
-                        frame.present();
-
-                        window.request_redraw();
-                    }
-                    WindowEvent::CloseRequested => target.exit(),
-                    _ => {}
-                };
-            }
-        })
-        .unwrap();
+                    window.request_redraw();
+                }
+                WindowEvent::CloseRequested => target.exit(),
+                _ => {}
+            };
+        }
+    })
 }
 
 #[cfg(target_arch = "wasm32")]
@@ -231,9 +231,11 @@ fn main() {
         .unwrap()
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .unwrap();
-    builder = builder.with_canvas(Some(canvas));
 
+    builder = builder.with_canvas(Some(canvas));
     let window = builder.build(&event_loop).unwrap();
+    info!("monitor scale factor: {}", window.scale_factor());
+    let _ = window.request_inner_size(winit::dpi::PhysicalSize::new(640, 640));
 
     wasm_bindgen_futures::spawn_local(run(event_loop, window));
 }

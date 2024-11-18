8257ba31256b75e2bd20ad9822f628c38ad0c153
Boris Dalstein <dalboris@gmail.com>
2024-11-18 18:45:41 +0100

    ui: Show and allow editing X/Y values in Objects panel via ArkUI NumberInput

--- packages/ui/src/ObjectsPanel.tsx
+++ packages/ui/src/ObjectsPanel.tsx
@@ -1,4 +1,5 @@
 import { Point, SceneManager } from './Scene.ts';
+import { NumberInput } from '@ark-ui/react/number-input';
 
 import './Panel.css';
 import './ObjectsPanel.css';
@@ -12,9 +13,33 @@ export function ObjectsPanel({ sceneManager }: ObjectsPanelProps) {
     <div className="panel">
       <h2 className="panel-title">Objects</h2>
       <div className="panel-body">
-        {sceneManager.scene().points.map((p: Point) => (
+        {sceneManager.scene().points.map((p: Point, index: number) => (
           <div className="object-row-info" key={p.name}>
-            <p>{p.name}</p>
+            <p className="object-name">{p.name}</p>
+            <NumberInput.Root
+              value={p.position.x}
+              onValueChange={d => {
+                sceneManager.scene().points[index].position.x = d.value;
+                sceneManager.commitChanges();
+              }}
+            >
+              <NumberInput.Scrubber>
+                <NumberInput.Label>X</NumberInput.Label>
+              </NumberInput.Scrubber>
+              <NumberInput.Input />
+            </NumberInput.Root>
+            <NumberInput.Root
+              value={p.position.y}
+              onValueChange={d => {
+                sceneManager.scene().points[index].position.y = d.value;
+                sceneManager.commitChanges();
+              }}
+            >
+              <NumberInput.Scrubber>
+                <NumberInput.Label>Y</NumberInput.Label>
+              </NumberInput.Scrubber>
+              <NumberInput.Input />
+            </NumberInput.Root>
           </div>
         ))}
       </div>

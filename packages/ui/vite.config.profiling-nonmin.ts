e52573475ca08574373f24cf81b4d9014beff8c0
Boris Dalstein <dalboris@gmail.com>
2024-11-20 15:13:53 +0100

    ui: Create different Vite configs for running the app with or without profiling and minimization

--- packages/ui/vite.config.profiling-nonmin.ts
+++ packages/ui/vite.config.profiling-nonmin.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    minifyIdentifiers: false,
+  },
+  resolve: {
+    alias: {
+      'react-dom/client': 'react-dom/profiling',
+    },
+  },
 });

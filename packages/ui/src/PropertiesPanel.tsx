5d2570ee2979e4a48dd87ad827085a0a534ee115
Boris Dalstein <dalboris@gmail.com>
2025-01-13 18:38:14 +0100

    ui: Show start and end points in Properties panel (#19)

--- packages/ui/src/PropertiesPanel.tsx
+++ packages/ui/src/PropertiesPanel.tsx
@@ -1,12 +1,27 @@
-import { Element, Point } from "./Document.ts";
+import {
+  ElementId,
+  Element,
+  Point,
+  EdgeElement,
+  LineSegment,
+  ArcFromStartTangent,
+  CCurve,
+  SCurve,
+} from "./Document.ts";
 import { DocumentManager } from "./DocumentManager.ts";
 import { NumberInput } from "./NumberInput.tsx";
+import { SkeletonListItem } from "./SkeletonListItem.tsx";
 
 interface PropertiesPanelProps {
   documentManager: DocumentManager;
 }
 
 export function PropertiesPanel({ documentManager }: PropertiesPanelProps) {
+  const doc = documentManager.document();
+  const selection = documentManager.selection();
+  const hoveredElementId = selection.hoveredElement();
+  const selectedElementIds = selection.selectedElements();
+
   const onXChange = (point: Point) => {
     return (value: number) => {
       point.position.x = value;
@@ -43,11 +58,62 @@ export function PropertiesPanel({ documentManager }: PropertiesPanelProps) {
     );
   }
 
-  function getContentForElement(element: Element) {
-    if (element.type === "Point") {
-      return getContentForPoint(element);
-    } else {
+  function getSkeletonItem(id: ElementId, title: string) {
+    const element = doc.getElementFromId<Point>(id);
+    if (!element) {
       return <></>;
+    }
+    return (
+      <SkeletonListItem
+        documentManager={documentManager}
+        id={id}
+        name={element.name}
+        isHovered={id === hoveredElementId}
+        isSelected={selectedElementIds.includes(id)}
+        title={title}
+      />
+    );
+  }
+
+  function getContentForEdge(edge: EdgeElement) {
+    return (
+      <>
+        {getSkeletonItem(edge.startPoint, "Start Point:")}
+        {getSkeletonItem(edge.endPoint, "End Point:")}
+      </>
+    );
+  }
+
+  function getContentForLineSegment(seg: LineSegment) {
+    return <>{getContentForEdge(seg)}</>;
+  }
+
+  function getContentForArcFromStartTangent(arc: ArcFromStartTangent) {
+    return <>{getContentForEdge(arc)}</>;
+  }
+
+  function getContentForCCurve(ccurve: CCurve) {
+    return <>{getContentForEdge(ccurve)}</>;
+  }
+
+  function getContentForSCurve(scurve: SCurve) {
+    return <>{getContentForEdge(scurve)}</>;
+  }
+
+  function getContentForElement(element: Element) {
+    switch (element.type) {
+      case "Point":
+        return getContentForPoint(element);
+      case "LineSegment":
+        return getContentForLineSegment(element);
+      case "ArcFromStartTangent":
+        return getContentForArcFromStartTangent(element);
+      case "CCurve":
+        return getContentForCCurve(element);
+      case "SCurve":
+        return getContentForSCurve(element);
+      case "Layer":
+        return <></>;
     }
   }
 

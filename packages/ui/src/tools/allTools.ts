4ffb072842d9fd43eeae6c6db92281b4bc7fec12
Boris Dalstein <dalboris@gmail.com>
2025-01-29 20:52:06 +0100

    ui: Implement minimal Line Segment tool (#20)

--- packages/ui/src/tools/allTools.ts
+++ packages/ui/src/tools/allTools.ts
@@ -1,6 +1,7 @@
 import { SelectTool } from "./SelectTool.ts";
 import { PointTool } from "./PointTool.ts";
+import { LineSegmentTool } from "./LineSegmentTool.ts";
 
 export function allTools() {
-  return [new SelectTool(), new PointTool()];
+  return [new SelectTool(), new PointTool(), new LineSegmentTool()];
 }

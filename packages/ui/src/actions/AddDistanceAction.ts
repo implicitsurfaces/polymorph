9ebf6f0fa3b89e808b52639b14c3bceae91fe29f
Boris Dalstein <dalboris@gmail.com>
2025-02-14 19:25:34 +0100

    ui: Implement 'Add Distance Measure' action (#84) (#92)

---
+++ packages/ui/src/actions/AddDistanceAction.ts
@@ -0,0 +1,51 @@
+import { Action } from "./Action";
+import { KeyboardShortcut } from "./KeyboardShortcut";
+import icon from "../assets/tool-icons/point-to-point-distance.svg";
+
+import { Point, Layer, PointToPointDistance } from "../Document";
+import { DocumentManager } from "../DocumentManager";
+
+export class AddDistanceAction implements Action {
+  readonly name = "Add Distance Measure";
+  readonly icon = icon;
+  readonly shortcut = new KeyboardShortcut("D");
+
+  constructor() {}
+
+  onTrigger(documentManager: DocumentManager) {
+    // Check that the selection is composed of exactly two points
+    const doc = documentManager.document();
+    const selection = documentManager.selection();
+    const layer = doc.getNode(selection.activeLayer(), Layer);
+    if (!layer) {
+      return "No active layer.";
+    }
+    const selectedNodes = selection.selectedNodes();
+    const points = doc.getNodes(selectedNodes, Point);
+    if (selectedNodes.length != 2 || points.length !== 2) {
+      return "Please select two points.";
+    }
+    const p1 = points[0].id;
+    const p2 = points[1].id;
+
+    // Check that a measure between these two points does not already exist
+    for (const node of doc.nodes()) {
+      if (node instanceof PointToPointDistance) {
+        if (
+          (node.startPoint === p1 && node.endPoint === p2) ||
+          (node.startPoint === p2 && node.endPoint === p1)
+        ) {
+          return "A point-to-point distance measure between these two points already exists.";
+        }
+      }
+    }
+
+    // Create the measure and set it as selection
+    const d = doc.createNode(PointToPointDistance, {
+      layer: layer,
+      startPoint: p1,
+      endPoint: p2,
+    });
+    selection.setSelectedNodes([d.id]);
+  }
+}
--- packages/ui/src/actions/allActions.ts
+++ packages/ui/src/actions/allActions.ts
@@ -1,5 +1,7 @@
 import { Action } from "./Action.ts";
 
+import { AddDistanceAction } from "./AddDistanceAction.ts";
+
 export function allActions(): Action[] {
-  return [];
+  return [new AddDistanceAction()];
 }

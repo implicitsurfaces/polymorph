c8870d08ddae5351ca0ed5555bd144369fa31266
Boris Dalstein <dalboris@gmail.com>
2025-03-19 11:29:43 +0100

    Re-order sketch CI check; Fix cannot lint sketch after building (#119) (#123)

--- packages/sketch/src/utils/cache.ts
+++ packages/sketch/src/utils/cache.ts
@@ -1,11 +1,9 @@
-export function memoizeNodeEval<
-  N extends object,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  T extends (node: N) => any,
->(fn: T): T {
-  const idCache = new WeakMap<N, ReturnType<T>>();
+export function memoizeNodeEval<N extends object, U>(
+  fn: (node: N) => U,
+): (node: N) => U {
+  const idCache = new WeakMap<N, U>();
 
-  return function (node: Parameters<T>[0]): ReturnType<T> {
+  return function (node: N): U {
     if (idCache.has(node)) {
       return idCache.get(node)!;
     }
@@ -14,5 +12,5 @@ export function memoizeNodeEval<
     idCache.set(node, val);
 
     return val;
-  } as T;
+  };
 }

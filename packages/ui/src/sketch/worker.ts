e7d2f7699323471c839270a622550bca40c23e6a
Boris Dalstein <dalboris@gmail.com>
2025-04-04 19:54:36 +0200

    Scale down size of document before render so that our test document fits in [-1, 1]

--- packages/ui/src/sketch/worker.ts
+++ packages/ui/src/sketch/worker.ts
@@ -1,13 +1,17 @@
 import { expose } from "comlink";
 import { initLib } from "fidget";
 
+import { Document } from "../doc/Document";
+import { ProfileNode } from "../doc/ProfileNode";
+import { Point } from "../doc/Point";
+
+import { drawProfileNode } from "./drawProfileNode";
+
+import { Vector2 } from "threejs-math";
+
 export interface SketchApi {
   render: (docJson: string, definition: number) => Promise<Uint8ClampedArray>;
 }
-
-import { Document } from "../doc/Document";
-import { ProfileNode } from "../doc/ProfileNode";
-import { drawProfileNode } from "./drawProfileNode";
 
 export function drawDocument(doc: Document) {
   // For now, we assume there is a single ProfileNode in the document,
@@ -32,9 +36,27 @@ const api: SketchApi = {
     docJson: string,
     definition: number,
   ): Promise<Uint8ClampedArray> => {
+    // Parse JSON
     const doc = Document.fromJSON(docJson);
-    await initLib();
+
+    // Scale down and invert Y axis
+    const scaleFactor = 0.005;
+    for (const node of doc.nodes()) {
+      if (node instanceof Point) {
+        const oldPos = node.position;
+        const newPos = new Vector2(
+          scaleFactor * oldPos.x,
+          -scaleFactor * oldPos.y,
+        );
+        node.position = newPos;
+      }
+    }
+
+    // Converts the document to a renderable ProfileEditor
     const value = drawDocument(doc);
+
+    // Perform the fidget render
+    await initLib();
     if (value) {
       return await value.render(definition);
     } else {

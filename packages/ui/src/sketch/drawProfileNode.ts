68d38906949d764ef6c2700492de67be19a41387
Boris Dalstein <dalboris@gmail.com>
2025-04-04 19:53:56 +0200

    Implement conversion of all edge types to sketch tree view draw-api

--- packages/ui/src/sketch/drawProfileNode.ts
+++ packages/ui/src/sketch/drawProfileNode.ts
@@ -1,8 +1,63 @@
 import { ProfileNode } from "../doc/ProfileNode";
 import { EdgeCycleProfile } from "../doc/profiles/EdgeCycleProfile";
-import { draw } from "draw-api";
+import { Halfedge } from "../doc/profiles/Halfedge";
 
-export function drawEdgeCycleProfile(node: EdgeCycleProfile) {
+import { LineSegment } from "../doc/edges/LineSegment";
+import { ArcFromStartTangent } from "../doc/edges/ArcFromStartTangent";
+import { CCurve } from "../doc/edges/CCurve";
+import { SCurve } from "../doc/edges/SCurve";
+
+import { draw, ProfileEditor } from "draw-api";
+
+// Note: in draw-api/vite.config.js, if dts.rollupTypes is true (which is the
+// case as of writing this), then PointMaker and EdgeMaker are not marked as
+// exported for some reason, and therefore we cannot do:
+//
+// import { PointMaker, EdgeMaker } from "draw-api"
+//
+// We use the following `any` aliases as a workaround.
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type EdgeMaker = any;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type PointMaker = any;
+
+function addEdge(d: EdgeMaker, halfedge: Halfedge): PointMaker {
+  const edge = halfedge.edge;
+  if (edge instanceof LineSegment) {
+    return d.line();
+  }
+  if (edge instanceof ArcFromStartTangent) {
+    if (halfedge.direction) {
+      return d.arcFromStartControl(edge.controlPoint.position);
+    } else {
+      return d.arcFromEndControl(edge.controlPoint.position);
+    }
+  }
+  if (edge instanceof CCurve) {
+    return d.CCurve(edge.controlPoint.position);
+  }
+  if (edge instanceof SCurve) {
+    if (halfedge.direction) {
+      return d.SCurve(
+        edge.startControlPoint.position,
+        edge.endControlPoint.position,
+      );
+    } else {
+      return d.SCurve(
+        edge.endControlPoint.position,
+        edge.startControlPoint.position,
+      );
+    }
+  }
+  // Fallback to  LineSegment
+  return d.line();
+}
+
+export function drawEdgeCycleProfile(
+  node: EdgeCycleProfile,
+): ProfileEditor | undefined {
   const halfedges = node.cycle.halfedges;
   const n = halfedges.length;
   if (n === 0) {
@@ -10,20 +65,19 @@ export function drawEdgeCycleProfile(node: EdgeCycleProfile) {
   }
   const startPoint = halfedges[0].startPoint();
   const lastIndex = n - 1;
-  let edgeMaker = draw(startPoint.position);
+  let d = draw(startPoint.position);
   for (const [i, halfedge] of halfedges.entries()) {
-    // TODO: handle all edge types instead of assuming line segment
-    const pointMaker = edgeMaker.line();
+    const e = addEdge(d, halfedge);
     if (i < lastIndex) {
-      edgeMaker = edgeMaker.line().to(halfedge.endPoint().position);
+      d = e.to(halfedge.endPoint().position);
     } else {
-      return pointMaker.close();
+      return e.close();
     }
   }
   return undefined;
 }
 
-export function drawProfileNode(node: ProfileNode) {
+export function drawProfileNode(node: ProfileNode): ProfileEditor | undefined {
   if (node instanceof EdgeCycleProfile) {
     return drawEdgeCycleProfile(node);
   }

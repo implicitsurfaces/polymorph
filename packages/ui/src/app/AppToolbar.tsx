56e875d6e2f4a043cee692e543600bacf5475749
Boris Dalstein <dalboris@gmail.com>
2025-04-22 22:55:55 +0200

    Fix indentation

--- packages/ui/src/app/AppToolbar.tsx
+++ packages/ui/src/app/AppToolbar.tsx
@@ -3,19 +3,19 @@ import { Toolbar, ToolbarMenu, ToolbarActionItem } from "../components/Toolbar";
 import { actions } from "./AppActions";
 
 export function AppToolbar() {
-	return (
-		<Toolbar>
-			<ToolbarMenu>
-				<MenuActionItem action={actions.Undo} />
-				<MenuActionItem action={actions.Redo} />
-				<MenuActionItem action={actions.Open} />
-				<MenuActionItem action={actions.Save} />
-				<MenuActionItem action={actions.SaveAs} />
-			</ToolbarMenu>
-			<ToolbarActionItem action={actions.SelectTool} />
-			<ToolbarActionItem action={actions.PointTool} />
-			<ToolbarActionItem action={actions.LineSegmentTool} />
-			<ToolbarActionItem action={actions.AddDistance} />
-		</Toolbar>
-	);
+  return (
+    <Toolbar>
+      <ToolbarMenu>
+        <MenuActionItem action={actions.Undo} />
+        <MenuActionItem action={actions.Redo} />
+        <MenuActionItem action={actions.Open} />
+        <MenuActionItem action={actions.Save} />
+        <MenuActionItem action={actions.SaveAs} />
+      </ToolbarMenu>
+      <ToolbarActionItem action={actions.SelectTool} />
+      <ToolbarActionItem action={actions.PointTool} />
+      <ToolbarActionItem action={actions.LineSegmentTool} />
+      <ToolbarActionItem action={actions.AddDistance} />
+    </Toolbar>
+  );
 }
